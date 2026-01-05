9b9372c84411c6c90368fa4bd944a6586460e7b7
August Bohmer <ab575@st-andrews.ac.uk>
2026-01-05 13:42:12 -0600

    180 days.

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ class Config:
     MIN_PROFIT_MARGIN_PERCENT: float = float(os.getenv('MIN_PROFIT_MARGIN_PERCENT', '10.0'))
     
     # Price Stability Thresholds
-    STABILITY_LOOKBACK_DAYS: int = int(os.getenv('STABILITY_LOOKBACK_DAYS', '90'))
+    STABILITY_LOOKBACK_DAYS: int = int(os.getenv('STABILITY_LOOKBACK_DAYS', '180'))
     MAX_AVG_DROP_DURATION_DAYS: int = int(os.getenv('MAX_AVG_DROP_DURATION_DAYS', '14'))
     DROP_THRESHOLD_PERCENT: float = float(os.getenv('DROP_THRESHOLD_PERCENT', '25.0'))
     
